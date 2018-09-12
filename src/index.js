d00eb5780c8a990641fa9d511fc912ee11674349
Zakhar Telyatnikov <tz@za-ek.ru>
2018-09-12 10:28:26 +0300

    Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 
+/***** CHECK REACT */
 class HelloMessage extends React.Component {
     render() {
         return (
@@ -15,3 +17,23 @@ ReactDOM.render(
     <HelloMessage name="Taylor Swift" />,
     document.getElementsByTagName('body')[0]
 );
+
+/***** CHECK REDUX */
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state
+    }
+}
+let store = createStore(counter);
+store.subscribe(() =>
+    console.log(store.getState())
+);
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+
